f1e7d4f95a0eba32ebb8c01234507f031148da8a
Wingscape <wingscape17@gmail.com>
2021-12-01 22:18:25 +0700

    Second Changes

--- face_recog.py
+++ face_recog.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
 
+# initialize Video Capture
 cap = cv2.VideoCapture(0)
 
 while(True):
+    # reading frame by frame, cause the loop as well
     ret, frame = cap.read()
 
+    # display the video frame
     cv2.imshow('frame', frame)
     
