eab81d9ed00dd17d9ad83c685b90076f9aee9b7c
Wingscape <wingscape17@gmail.com>
2021-12-01 23:38:49 +0700

    The camera is showing up really well

--- face_recog.py
+++ face_recog.py
@@ -1,13 +1,20 @@
 import numpy as np
 import cv2
 
-# initialize Video Capture
+# initialize video capture
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # reading frame by frame, cause the loop as well
+    # reading frame by frame, cause of the loop as well
     ret, frame = cap.read()
 
     # display the video frame
     cv2.imshow('frame', frame)
+
+    # stop the program when 'q' is pressed
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
     
