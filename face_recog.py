29b2046ad73a16522ba3a488a339ff0838f99348
Wingscape <wingscape17@gmail.com>
2021-12-02 09:18:21 +0700

    Declare and define the camera resolution

--- face_recog.py
+++ face_recog.py
@@ -1,8 +1,19 @@
 import numpy as np
 import cv2
 
+std_dimensions = {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080)
+}
+
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
 # initialize video capture
 cap = cv2.VideoCapture(0)
+change_res(cap, std_dimensions["480p"])
 
 while(True):
     # reading frame by frame, cause of the loop as well
