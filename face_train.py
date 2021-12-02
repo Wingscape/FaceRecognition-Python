5c1a887b215fd2cfe1b86fa951482d02ba1c9acd
Wingscape <wingscape17@gmail.com>
2021-12-02 17:49:18 +0700

    giving id and label to pictures

--- face_train.py
+++ face_train.py
@@ -1,4 +1,7 @@
 import os
+import cv2
+import numpy as np
+from PIL import Image
 
 # get the current directory of the file
 # os.path.dirname is used to get the directory
@@ -6,6 +9,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # get directory of Images folder
 image_dir = os.path.join(BASE_DIR, "Images")
+
+face_cascade = cv2.CascadeClassifier('Cascades/data/haarcascade_frontalface_alt2.xml')
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
 
 # separate file so that only picture that get taken
 # root is top-most directory in a hierarchy
@@ -18,6 +28,27 @@ for root, dirs, files in os.walk(image_dir):
             # make a label from the foldername of the file
             # basename is to get the folder of the path
             label = os.path.basename(root).replace(" ", "-").lower()
+            print(label, path)
 
-            print(path)
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
 
+            id_ = label_ids[label]
+            print(label_ids)
+
+            # pillow is python image library
+            # open image of the directory and convert it into grayscale
+            pil_image = Image.open(path).convert("L")
+
+            # change image into NUMPY array
+            image_array = np.array(pil_image, "uint8")
+            print(image_array)
+
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+
+                x_train.append(roi)
+                y_labels.append(id_)
